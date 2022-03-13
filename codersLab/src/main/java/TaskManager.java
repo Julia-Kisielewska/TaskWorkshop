542a906d67117a66019a9d11c581b48452ddf27f
Julia Kisielewska <julia.kisielewska@gmail.com>
2022-03-13 19:33:33 +0100

    20220313_Warsztat -skończone zadania- pytania do weryfikacji

--- codersLab/src/main/java/TaskManager.java
+++ codersLab/src/main/java/TaskManager.java
@@ -1,7 +1,12 @@
 package coderslab;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import pl.coderslab.ConsoleColors;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -9,60 +14,58 @@ import java.util.Scanner;
 public class TaskManager {
     public static void main(String[] args) {
 
-        System.out.println(pl.coderslab.ConsoleColors.BLUE + "Please select an option:");
-
-        String[] options = {
-                "add",
-                "remove",
-                "list",
-                "exit"
-        };
-        for (int i = 0; i < options.length; i++) {
-            System.out.println(pl.coderslab.ConsoleColors.WHITE_BRIGHT + options[i]);
-
-        }
+//Method01- display the list of available options regarding the color.
         String[][] tasks = dataInput();
-        // for (int i = 0; i < tasks.length; i++) {
-        //     for (int j = 0; j < tasks[i].length; j++) {
-        //        System.out.println(tasks[i][j]);
-        //     }
-        // }
+        optionsList();
 
-        //System.out.println(tasks[0][1]);
-        // System.out.println(tasks[1][1]);
-        //System.out.println(tasks[2][1]);
+//Method02- obtain a 2D array from the csv file.
 
+
+//scan the options.
         Scanner scan = new Scanner(System.in);
-        String input = scan.next();
+        while (scan.hasNextLine()) {
+            String input = scan.next();
 
         switch (input) {
-            case "add":
-                String[][] tasksAdd = addTask();
-                break;
-            case "remove":
-                System.out.println("Usuń zadanie");
-                break;
-            case "list":
-                String lista = displayList();
-                System.out.println(lista);
-                break;
-            case "exit":
-                break;
-            default:
-                System.out.println("Please select a correct option.");
+                case "add":
+//Method03- add tasks, obtain new array.
+                    String[][] tasksAdd = addTask();
+                    break;
+                case "remove":
+//Method04- remove tasks
+                    removeLine(tasks, numberObtain());
+                    System.out.println("Value was succesfully deleted");
+                    for (int i = 0; i < tasks.length; i++) {
+                        for (int j = 0; j < tasks[i].length; j++) {
+                            System.out.println(tasks[i][j]);
+                        }
+                    }
+                    break;
+//Method05- display list
+                case "list":
+                    displayList(tasks);
+                    break;
+                case "exit":
+                    saveFile();
+                    System.out.println(ConsoleColors.RED + "Bye, bye!");
+                    break;
+                default:
+                    System.out.println("Please select a correct option.");
+            }
+            optionsList();
         }
     }
 
+    //Method03
     public static String[][] addTask() {
         Scanner scanTask = new Scanner(System.in);
-
         System.out.println("Please add task description");
         String inputTask = scanTask.next();
-        System.out.println(inputTask);
+        //System.out.println(inputTask);
 
         System.out.println("Please add task due date (yyyy-mm-dd");
         String date = scanTask.next();
-        System.out.println(date);
+        //System.out.println(date);
 
 
         System.out.println("Is your task important: true/false");
@@ -87,24 +90,70 @@ public class TaskManager {
         return tasks2;
     }
 
-    public static String displayList() {
-        String[][] tasks = dataInput();
-        StringBuilder sb = new StringBuilder();
-        StringBuilder inner = new StringBuilder();
-        for (int i = 0; i < tasks.length; i++) {
-            for (int j = 0; j < tasks[i].length; j++) {
-                inner.append(tasks[i][j] + " ");
+    //Method05
+    public static void displayList(String[][] tab) {
+        //String[][] tasks = dataInput();
+        //StringBuilder sb = new StringBuilder();
+        //StringBuilder inner = new StringBuilder();
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print(i + " : ");
+            for (int j = 0; j < tab[i].length; j++) {
+                System.out.print(tab[i][j] + " ");
             }
-            sb.append(i + " : " + inner.toString() + "\n");
+            System.out.println();
         }
-
-        String display = sb.toString();
-        return display;
     }
 
+    //Method04a
+    public static void removeLine(String[][] tab, int index) {
+        try {
+            if (index < tab.length) {
+                String[][]tasks = ArrayUtils.remove(tab, index);
+        }
+    } catch(ArrayIndexOutOfBoundsException ex){
+                System.out.println("Element doesn't exist");
+            }
+        }
+
+    //Method04b.1
+    public static int numberObtain() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please select number to remove");
+
+        String number = scan.nextLine();
+        while (!isNumberGreaterEqualZero(number)) {
+            System.out.println("Incorrect argument passed. Please give number greater or equal 0.");
+            scan.nextLine();
+        }
+        return Integer.parseInt(number);
+    }
+
+    //Method04b.1
+    public static boolean isNumberGreaterEqualZero(String input) {
+        if (NumberUtils.isParsable(input)) {
+            return Integer.parseInt(input) >= 0;
+        }
+        return false;
+    }
+
+    public static void saveFile() {
+        String[][] tasks = dataInput();
+        try (PrintWriter printWriter = new PrintWriter("tasks01.csv")) {
+            for (int i = 0; i < tasks.length; i++) {
+                for (int j = 0; j < tasks[i].length; j++) {
+                    printWriter.println(tasks[i][j]);
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Błąd zapisu do pliku.");
+        }
+
+    }
+
+    //Method02
     public static String[][] dataInput() {
 
-        // File file = new File(path);
+//add a file and convert it into the String
         File file = new File("tasks.csv");
         StringBuilder reading = new StringBuilder();
         try {
@@ -118,7 +167,8 @@ public class TaskManager {
         //System.out.println(reading.toString());
 
         String fromFile = reading.toString();
-        //System.out.println(fromFile);
+
+        //Convert the String into a 2D array
 
         String[] outer = fromFile.split("\\r?\\n");
         int size = outer.length;
@@ -131,5 +181,21 @@ public class TaskManager {
         return result;
         //System.out.println("TEST");
         // System.out.println(result[0][0]);
+    }
+
+
+    //Method01
+    public static void optionsList() {
+        System.out.println(pl.coderslab.ConsoleColors.BLUE + "Please select an option:");
+
+        String[] options = {
+                "add",
+                "remove",
+                "list",
+                "exit"
+        };
+        for (int i = 0; i < options.length; i++) {
+            System.out.println(pl.coderslab.ConsoleColors.WHITE_BRIGHT + options[i]);
+        }
     }
 }
